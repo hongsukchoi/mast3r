e1439ee0eb320554543a76d502099610302c85db
hongsukchoi <redstonepo@gmail.com>
2024-10-13 00:59:18 +0000

    update croco path

--- mast3r/catmlp_dpt_head.py
+++ mast3r/catmlp_dpt_head.py
@@ -10,8 +10,8 @@ import torch.nn.functional as F
 # import mast3r.utils.path_to_dust3r  # noqa
 from dust3r.heads.postprocess import reg_dense_depth, reg_dense_conf  # noqa
 from dust3r.heads.dpt_head import PixelwiseTaskWithDPT  # noqa
-import dust3r.utils.path_to_croco  # noqa
-from models.blocks import Mlp  # noqa
+# import dust3r.utils.path_to_croco  # noqa
+from croco.models.blocks import Mlp  # noqa
 
 
 def reg_desc(desc, mode):
